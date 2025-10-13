621270547ded948de143be2d97b5f2a9856f8cd2
Mikal Paudel <mikalpaudel58@gmail.com>
2025-10-13 20:00:23 +0545

    Name the days and months that is given number in inbuilt function

--- script.js
+++ script.js
@@ -5,8 +5,10 @@ const year= document.getElementById('year');
 
 const today = new Date();
 
+const weekDays = ['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday'];
+const allMonths = ['January', 'February','March','April','May','June','July','August','September','October','November','December'];
 
 date.innerHTML = today.getDate();
-day.innerHTML =today.getDay();
-month.innerHTML =today.getMonth();
+day.innerHTML = weekDays[today.getDay()];
+month.innerHTML = allMonths[today.getMonth()];
 year.innerHTML =today.getFullYear();
