be1bd8692abe271df2e954c044eb230908ed8e86
Mikal Paudel <mikalpaudel58@gmail.com>
2025-10-13 20:02:49 +0545

    Add some CSS in displayed day n date with 0 in front if single digit.

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ const today = new Date();
 const weekDays = ['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday'];
 const allMonths = ['January', 'February','March','April','May','June','July','August','September','October','November','December'];
 
-date.innerHTML = today.getDate();
+date.innerHTML = (today.getDate()<10?'0':'') + today.getDate();
 day.innerHTML = weekDays[today.getDay()];
 month.innerHTML = allMonths[today.getMonth()];
 year.innerHTML =today.getFullYear();
